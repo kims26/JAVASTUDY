702cf00c16a0673fbe5b2d8ff8e0f1f5de304ac1
김상현 <114653761+kims26@users.noreply.github.com>
2023-07-17 18:07:00 +0900

    Add files via upload

---
+++ 2023_0717_MVCFramework/src/main/java/action/TestAction2.java
@@ -0,0 +1,27 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import annotation.RequestMapping;
+import annotation.ResponseBody;
+
+public class TestAction2 {
+
+	
+	
+	@RequestMapping(value="/test2.do",produces="text/json;charset=utf-8;")
+	@ResponseBody
+	public String json(HttpServletRequest request,HttpServletResponse response) {
+		
+		return "{\"result\":\"test2222\"}";
+	}
+	
+	
+	@RequestMapping("/test3.do")
+	public String test3(HttpServletRequest request, HttpServletResponse response) {
+
+		return "xxx.jsp";
+	}
+	
+}
